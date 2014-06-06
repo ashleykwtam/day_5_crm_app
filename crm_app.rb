ea6fe822426653b7074b1b545f8475c9e6824396
Ashley <ashleykwtam@gmail.com>
2014-06-06 09:29:11 -0400

    fixed error to run CRM

--- crm_app.rb
+++ crm_app.rb
@@ -4,17 +4,22 @@ require_relative "rolodex"
 class CRM
 
 	def self.run(name)			# self is a class method
-		crm = CRM.new("name")
+		crm = CRM.new(name)
 		crm.main_menu
 	end
 
+	def initialize(name)		# need an initialize method to call on name
+		@name = name
+	end
+
 	def main_menu
-		print_main_menu
+		puts print_main_menu
 		user_selected = gets.to_i			# stores a number into user_selected
 		call_option(user_selected)		# variable passed into a method called call_option
 	end
 
 	def print_main_menu
+		puts "Choose the following action for your contact book:"
 		puts "[1] Add a new contact"
 		puts "[2] Modify an existing contact"
 		puts "[3] Display all contacts"
@@ -22,7 +27,6 @@ class CRM
 		puts "[5] Display a certain attribute"
 		puts "[6] Delete a contact"
 		puts "[7] Exit"
-		puts "Enter a number: "
 	end
 
 	def call_option(user_selected)		# this method calls on other methods based on the number
@@ -37,9 +41,4 @@ class CRM
 end
 
 
-
-a_crm_app = CRM.new
-a_crm_app.print_main_menu
-
-adrian = Contact.new("Mr", "Adrian", "Chan", "achan@gmail.com", "my bruh")
 CRM.run("my CRM")
