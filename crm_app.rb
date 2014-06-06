836198fa759b8537cd3d8b26cf61fd8f3be4c3aa
Ashley <ashleykwtam@gmail.com>
2014-06-05 23:36:17 -0400

    defined attributes and initialized them in contact class

--- crm_app.rb
+++ crm_app.rb
@@ -28,7 +28,12 @@ class CRM
 end
 
 class Contact < CRM
-	def initialize
+	def initialize(id, first_name, last_name, email, note)
+		@id = id
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@note = note
 	end
 
 	def add_new_contact
