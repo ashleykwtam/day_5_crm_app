b8b9b5f122700ca93da86b4a62a753f9e7f3c72c
Ashley <ashleykwtam@gmail.com>
2014-06-05 23:03:03 -0400

    defined print main menu inside class CRM

--- crm_app.rb
+++ crm_app.rb
@@ -3,6 +3,14 @@ class CRM
 	end
 
 	def print_main_menu
+		puts "[1] Add a new contact"
+		puts "[2] Modify an existing contact"
+		puts "[3] Display all contacts"
+		puts "[4] Display one contact"
+		puts "[5] Display a certain attribute"
+		puts "[6] Delete a contact"
+		puts "[7] Exit"
+		puts "Enter a number: "
 	end
 
 	def call_option(user_selected)
@@ -15,3 +23,6 @@ end
 
 class Rolodex
 end
+
+a_crm_app = CRM.new
+a_crm_app.print_main_menu
